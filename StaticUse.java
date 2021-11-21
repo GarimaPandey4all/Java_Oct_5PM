ddc6d93684752ab9f25075cb0c6fc2fa52b5ad7a
Garima Pandey <59961721+GarimaPandey4all@users.noreply.github.com>
2021-11-21 16:27:52 +0530

    Add files via upload

---
+++ StaticUse.java
@@ -0,0 +1,31 @@
+package com.brainmentors.java.oops;
+
+class Enemy
+{
+	private String name;
+	private boolean weapon;
+	private int attack;
+	static int counter; // static variable
+	
+	Enemy(String name, boolean weapon, int attack)
+	{
+		this.name = name;
+		this.weapon = weapon;
+		this.attack = attack;
+		counter++;
+		System.out.println("Enemy Created "+counter+" "+name+" "+weapon+" "+attack);
+		//System.out.println("Enemy Created "+counter);
+	}
+}
+
+public class StaticUse {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		for(int i = 1; i <= 10; i++)
+		{
+			Enemy obj = new Enemy("L"+i, true, i);
+		}
+	}
+}
